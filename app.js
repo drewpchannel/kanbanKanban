13b178845a57acddaae4777d87f6fa14fc8af228
SDodson <siddodson@gmail.com>
2016-07-28 19:42:29 -1000

    File Structure set up

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("You suk");
+'use strict'
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+
+
+
+/*  ROUTES  */
+var routr = require ('./routes/router');
+app.use('/gallery', routr);
+app.use('/', routr);
+app.use(express.static('public'));
+
+
+app.listen(PORT, function() {
+  db.sequelize.sync();
+});
